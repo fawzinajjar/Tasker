712748f618f37e83b3d948e00a2e257873088987
Assaad Najjar <rajjix@rajjix.com>
2020-08-29 15:55:38 +0300

    fix axios headers

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -1,35 +1,35 @@
-import axios from "axios";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
-import { setAlert } from "./alert";
+import axios from 'axios';
+import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import { setAlert } from './alert';
 
 export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
     headers: {
-      "content-type": "application.json",
-    },
+      'Content-Type': 'application/json'
+    }
   };
   const body = { name, email, password };
-  console.log("body: ", body);
+  console.log('body: ', body);
   try {
     const res = await axios.post(
-      "http://localhost:5000/register",
+      'http://localhost:5000/register/',
       body,
       config
     );
     dispatch({
       type: REGISTER_SUCCESS,
-      payload: res.data,
+      payload: res.data
     });
   } catch (error) {
     const errors = error.response.data.errors;
 
     if (errors) {
       errors.forEach((error) => {
-        dispatch(setAlert(error.msg), "danger");
+        dispatch(setAlert(error.msg), 'danger');
       });
     }
   }
   dispatch({
-    type: REGISTER_FAIL,
+    type: REGISTER_FAIL
   });
 };
